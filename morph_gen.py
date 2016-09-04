854d0927d5a044a338966c85571552e53a7ccb4a
Francis Tyers <francis.tyers@uit.no>
2016-09-04 13:43:39 +0200

    make exe

--- morph_gen.py
+++ morph_gen.py
@@ -1,4 +1,13 @@
+"""Learn to reverse the letters in each word in text
+
+In this demo, a recurrent network equipped with an attention mechanism
+learns to reverse each word (on a character-by-character basis) in 
+its input text. 
+"""
+
 from __future__ import print_function
+import argparse
+import glob
 import logging
 import pprint
 import math
@@ -336,3 +345,33 @@ def main(mode, save_path, num_batches, data_path=None):
             messages.sort(key=operator.itemgetter(0), reverse=True)
             for _, message in messages:
                 print(message)
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s: %(name)s: %(levelname)s: %(message)s")
+    parser = argparse.ArgumentParser(
+        "Case study of learning to reverse words in a text.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "mode", choices=["train", "sample", "beam_search"],
+        help="The mode to run. In the `train` mode a model is trained."
+             " In the `sample` and `beam_search` modes a trained model is "
+             " to used reverse words in the input text.")
+    parser.add_argument(
+        "save_path", default="reverse_words.tar", nargs='?',
+        help="The path to save the training process if the mode"
+             " is `train` OR path to an `.tar` files with learned"
+             " parameters if the mode is `test`.")
+    parser.add_argument(
+        "--num-batches", default=10000, type=int,
+        help="Train on this many batches.")
+    parser.add_argument(
+        "--data-path",
+        help="text file(s) to read for training, with bash-like expansion"
+             " so wildchars can be used (e.g. data/*.txt)")
+    args = parser.parse_args()
+    if args.data_path:
+        args.data_path = glob.glob(args.data_path)
+    main(**vars(args))
+
