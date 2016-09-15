a63d7f4818d15c77314f1eae3d4bfd25edb3e0d1
Francis Tyers <francis.tyers@uit.no>
2016-09-15 12:05:46 +0200

    start rewrite

--- morph_gen.py
+++ morph_gen.py
@@ -1,24 +1,12 @@
-"""Learn to reverse the letters in each word in text
+"""
+Learn to morphologically generate surface forms from analyses
 
 In this demo, a recurrent network equipped with an attention mechanism
-learns to reverse each word (on a character-by-character basis) in 
-its input text. 
+learns to morphologically generate each word (on a symbol-by-symbol basis) in
+its input text.
 """
 
-from __future__ import print_function
-import argparse
-import glob
-import logging
-import pprint
-import math
-import numpy
-import traceback
-import operator
-
-import theano
-from six.moves import input
-from picklable_itertools.extras import equizip
-from theano import tensor
+import sys;
 
 from blocks.bricks import Tanh, Initializable
 from blocks.bricks.base import application
@@ -26,352 +14,68 @@ from blocks.bricks.lookup import LookupTable
 from blocks.bricks.recurrent import SimpleRecurrent, Bidirectional
 from blocks.bricks.attention import SequenceContentAttention
 from blocks.bricks.parallel import Fork
-from blocks.bricks.sequence_generators import (
-    SequenceGenerator, Readout, SoftmaxEmitter, LookupFeedback)
-from blocks.config import config
-from blocks.graph import ComputationGraph
-from fuel.transformers import Mapping, Batch, Padding, Filter
-from fuel.datasets import TextFile
-from fuel.schemes import ConstantScheme
-from blocks.serialization import load_parameters
-from blocks.algorithms import (GradientDescent, Scale,
-                               StepClipping, CompositeRule)
-from blocks.initialization import Orthogonal, IsotropicGaussian, Constant
-from blocks.model import Model
-from blocks.monitoring import aggregation
-from blocks.extensions import FinishAfter, Printing, Timing
-from blocks.extensions.saveload import Checkpoint
-from blocks.extensions.monitoring import TrainingDataMonitoring
-from blocks.main_loop import MainLoop
-from blocks.filter import VariableFilter
-from blocks.utils import dict_union
-
-from blocks.search import BeamSearch
-
-config.recursion_limit = 100000
-floatX = theano.config.floatX
-logger = logging.getLogger(__name__)
-
-# Dictionaries
-all_chars = (['a', 'c', 'e', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'] + [',', '.', '!', '?', '<UNK>'] + [' ', '<S>', '</S>'])
-#all_chars = ([chr(ord('a') + i) for i in range(26)] +
-#             [chr(ord('0') + i) for i in range(10)] +
-#             [',', '.', '!', '?', '<UNK>'] +
-#             [' ', '<S>', '</S>'])
-code2char = dict(enumerate(all_chars))
-char2code = {v: k for k, v in code2char.items()}
+from blocks.bricks.sequence_generators import (SequenceGenerator, Readout, SoftmaxEmitter, LookupFeedback)
 
 
-def reverse_words(sample):
-    sentence = sample[0]
-    result = []
-    word_start = -1
-    print('@@@ IN :', sample);
-    for i, code in enumerate(sentence):
-        if code >= char2code[' ']:
-            if word_start >= 0:
-                result.extend(sentence[i - 1:word_start - 1:-1])
-                word_start = -1
-            result.append(code)
-        else:
-            if word_start == -1:
-                word_start = i
-    print('@@@ OUT:', result);
-    return (result,)
 
+class Globals: #{
 
-def _lower(s):
-    return s.lower()
+	char2code = {};
+	code2char = {};
+	lookup = {};
 
+	def read_alphabet(path): #{
+		f = open(path);
+		vocab = f.readline().strip();
+		all_chars = (vocab.split(' ') + ['<UNK>']);
+		Globals.code2char = dict(enumerate(all_chars))
+		Globals.char2code = {v: k for k, v in Globals.code2char.items()}
+	#}
+#}
 
-def _transpose(data):
-    return tuple(array.T for array in data)
+class MorphGen: #{
 
+	def __init__(self, dimen, vocab_size): #{
+		
+		# The encoder 
+	        encoder = Bidirectional(SimpleRecurrent(dim=dimen, activation=Tanh()))
 
-def _filter_long(data):
-    return len(data[0]) <= 100
+		# What is this doing ? 
+	        fork = Fork([name for name in encoder.prototype.apply.sequences if name != 'mask'])
+	        fork.input_dim = dimen
+	        fork.output_dims = [encoder.prototype.get_dim(name) for name in fork.input_names]
 
+	        lookup = LookupTable(vocab_size, dimen)
+	        transition = SimpleRecurrent(activation=Tanh(),dim=dimen, name="transition")
+	        attention = SequenceContentAttention(state_names=transition.apply.states,attended_dim=2*dimen, match_dim=dimen, name="attention")
+	        readout = Readout(
+	            readout_dim=vocab_size,
+	            source_names=[transition.apply.states[0],
+	                          attention.take_glimpses.outputs[0]],
+	            emitter=SoftmaxEmitter(name="emitter"),
+	            feedback_brick=LookupFeedback(vocab_size, dimen),
+	            name="readout")
+	        generator = SequenceGenerator(readout=readout, transition=transition, attention=attention,name="generator")
+	
+	        self.lookup = lookup
+	        self.fork = fork
+	        self.encoder = encoder
+	        self.generator = generator
+	        self.children = [lookup, fork, encoder, generator]
+	
+	
+	#}
+#}
 
-def _is_nan(log):
-    return math.isnan(log.current_row['total_gradient_norm'])
+if len(sys.argv) < 2: #{
+	sys.exit(-1);
+#}
 
+Globals.read_alphabet(sys.argv[1]);
 
-class WordReverser(Initializable):
-    """The top brick.
-
-    It is often convenient to gather all bricks of the model under the
-    roof of a single top brick.
-
-    """
-    def __init__(self, dimension, alphabet_size, **kwargs):
-        super(WordReverser, self).__init__(**kwargs)
-        encoder = Bidirectional(
-            SimpleRecurrent(dim=dimension, activation=Tanh()))
-        fork = Fork([name for name in encoder.prototype.apply.sequences
-                    if name != 'mask'])
-        fork.input_dim = dimension
-        fork.output_dims = [encoder.prototype.get_dim(name) for name in fork.input_names]
-        lookup = LookupTable(alphabet_size, dimension)
-        transition = SimpleRecurrent(
-            activation=Tanh(),
-            dim=dimension, name="transition")
-        attention = SequenceContentAttention(
-            state_names=transition.apply.states,
-            attended_dim=2 * dimension, match_dim=dimension, name="attention")
-        readout = Readout(
-            readout_dim=alphabet_size,
-            source_names=[transition.apply.states[0],
-                          attention.take_glimpses.outputs[0]],
-            emitter=SoftmaxEmitter(name="emitter"),
-            feedback_brick=LookupFeedback(alphabet_size, dimension),
-            name="readout")
-        generator = SequenceGenerator(
-            readout=readout, transition=transition, attention=attention,
-            name="generator")
-
-        self.lookup = lookup
-        self.fork = fork
-        self.encoder = encoder
-        self.generator = generator
-        self.children = [lookup, fork, encoder, generator]
-
-    @application
-    def cost(self, chars, chars_mask, targets, targets_mask):
-        return self.generator.cost_matrix(
-            targets, targets_mask,
-            attended=self.encoder.apply(
-                **dict_union(
-                    self.fork.apply(self.lookup.apply(chars), as_dict=True),
-                    mask=chars_mask)),
-            attended_mask=chars_mask)
-
-    @application
-    def generate(self, chars):
-        return self.generator.generate(
-            n_steps=3 * chars.shape[0], batch_size=chars.shape[1],
-            attended=self.encoder.apply(
-                **dict_union(
-                    self.fork.apply(self.lookup.apply(chars), as_dict=True))),
-            attended_mask=tensor.ones(chars.shape))
-
-
-def main(mode, save_path, num_batches, data_path=None):
-    reverser = WordReverser(100, len(char2code), name="reverser")
-
-    print('@@@ main', mode, save_path, data_path);
-
-    if mode == "train":
-        # Data processing pipeline
-        print('@@@ "train":');
-        dataset_options = dict(dictionary=char2code, level="character",
-                               preprocess=_lower)
-        if data_path:
-            dataset = TextFile(data_path, **dataset_options)
-            print('@@@ "data_file":', data_path);
-
-        data_stream = dataset.get_example_stream()
-        data_stream = Filter(data_stream, _filter_long)
-        data_stream = Mapping(data_stream, reverse_words,
-                              add_sources=("targets",))
-        data_stream = Batch(data_stream, iteration_scheme=ConstantScheme(10))
-        data_stream = Padding(data_stream)
-        data_stream = Mapping(data_stream, _transpose)
-
-        # Initialization settings
-        reverser.weights_init = IsotropicGaussian(0.1)
-        reverser.biases_init = Constant(0.0)
-        reverser.push_initialization_config()
-        reverser.encoder.weights_init = Orthogonal()
-        reverser.generator.transition.weights_init = Orthogonal()
-
-        # Build the cost computation graph
-        chars = tensor.lmatrix("features")
-        chars_mask = tensor.matrix("features_mask")
-        targets = tensor.lmatrix("targets")
-        targets_mask = tensor.matrix("targets_mask")
-        batch_cost = reverser.cost(
-            chars, chars_mask, targets, targets_mask).sum()
-        batch_size = chars.shape[1].copy(name="batch_size")
-        cost = aggregation.mean(batch_cost, batch_size)
-        cost.name = "sequence_log_likelihood"
-        logger.info("Cost graph is built")
-
-        # Give an idea of what's going on
-        model = Model(cost)
-        parameters = model.get_parameter_dict()
-        logger.info("Parameters:\n" +
-                    pprint.pformat(
-                        [(key, value.get_value().shape) for key, value
-                         in parameters.items()],
-                        width=120))
-
-        # Initialize parameters
-        for brick in model.get_top_bricks():
-            brick.initialize()
-
-        # Define the training algorithm.
-        cg = ComputationGraph(cost)
-        algorithm = GradientDescent(
-            cost=cost, parameters=cg.parameters,
-            step_rule=CompositeRule([StepClipping(10.0), Scale(0.01)]))
-
-        # Fetch variables useful for debugging
-        generator = reverser.generator
-        (energies,) = VariableFilter(
-            applications=[generator.readout.readout],
-            name_regex="output")(cg.variables)
-        (activations,) = VariableFilter(
-            applications=[generator.transition.apply],
-            name=generator.transition.apply.states[0])(cg.variables)
-        max_length = chars.shape[0].copy(name="max_length")
-        cost_per_character = aggregation.mean(
-            batch_cost, batch_size * max_length).copy(
-                name="character_log_likelihood")
-        min_energy = energies.min().copy(name="min_energy")
-        max_energy = energies.max().copy(name="max_energy")
-        mean_activation = abs(activations).mean().copy(
-                name="mean_activation")
-        observables = [
-            cost, min_energy, max_energy, mean_activation,
-            batch_size, max_length, cost_per_character,
-            algorithm.total_step_norm, algorithm.total_gradient_norm]
-        for name, parameter in parameters.items():
-            observables.append(parameter.norm(2).copy(name + "_norm"))
-            observables.append(algorithm.gradients[parameter].norm(2).copy(
-                name + "_grad_norm"))
-
-        # Construct the main loop and start training!
-        average_monitoring = TrainingDataMonitoring(
-            observables, prefix="average", every_n_batches=10)
+print(Globals.char2code);
+print(Globals.code2char);
         
-        main_loop = MainLoop(
-            model=model,
-            data_stream=data_stream,
-            algorithm=algorithm,
-            extensions=[
-                Timing(),
-                TrainingDataMonitoring(observables, after_batch=True),
-                average_monitoring,
-                FinishAfter(after_n_batches=num_batches)
-                # This shows a way to handle NaN emerging during
-                # training: simply finish it.
-                .add_condition(["after_batch"], _is_nan),
-                # Saving the model and the log separately is convenient,
-                # because loading the whole pickle takes quite some time.
-                Checkpoint(save_path, every_n_batches=500,
-                           save_separately=["model", "log"]),
-                Printing(every_n_batches=1)])
-        main_loop.run()
-    elif mode == "sample" or mode == "beam_search":
-        chars = tensor.lmatrix("input")
-        generated = reverser.generate(chars)
-        model = Model(generated)
-        logger.info("Loading the model..")
-        with open(save_path, 'rb') as f:
-            model.set_parameter_values(load_parameters(f))
+m = MorphGen(100, len(Globals.char2code));
 
-        def generate(input_):
-            """Generate output sequences for an input sequence.
-
-            Incapsulates most of the difference between sampling and beam
-            search.
-
-            Returns
-            -------
-            outputs : list of lists
-                Trimmed output sequences.
-            costs : list
-                The negative log-likelihood of generating the respective
-                sequences.
-
-            """
-            if mode == "beam_search":
-                samples, = VariableFilter(
-                    applications=[reverser.generator.generate], name="outputs")(
-                        ComputationGraph(generated[1]))
-                # NOTE: this will recompile beam search functions
-                # every time user presses Enter. Do not create
-                # a new `BeamSearch` object every time if
-                # speed is important for you.
-                beam_search = BeamSearch(samples)
-                outputs, costs = beam_search.search(
-                    {chars: input_}, char2code['</S>'],
-                    3 * input_.shape[0])
-            else:
-                _1, outputs, _2, _3, costs = (
-                    model.get_theano_function()(input_))
-                outputs = list(outputs.T)
-                costs = list(costs.T)
-                for i in range(len(outputs)):
-                    outputs[i] = list(outputs[i])
-                    try:
-                        true_length = outputs[i].index(char2code['</S>']) + 1
-                    except ValueError:
-                        true_length = len(outputs[i])
-                    outputs[i] = outputs[i][:true_length]
-                    costs[i] = costs[i][:true_length].sum()
-            return outputs, costs
-
-        while True:
-            try:
-                line = input("Enter a sentence\n")
-                message = ("Enter the number of samples\n" if mode == "sample"
-                        else "Enter the beam size\n")
-                batch_size = int(input(message))
-            except EOFError:
-                break
-            except Exception:
-                traceback.print_exc()
-                continue
-
-            encoded_input = [char2code.get(char, char2code["<UNK>"])
-                             for char in line.lower().strip()]
-            encoded_input = ([char2code['<S>']] + encoded_input +
-                             [char2code['</S>']])
-            print("Encoder input:", encoded_input)
-            target = reverse_words((encoded_input,))[0]
-            print("Target: ", target)
-
-            samples, costs = generate(
-                numpy.repeat(numpy.array(encoded_input)[:, None],
-                             batch_size, axis=1))
-            messages = []
-            for sample, cost in equizip(samples, costs):
-                message = "({})".format(cost)
-                message += "".join(code2char[code] for code in sample)
-                if sample == target:
-                    message += " CORRECT!"
-                messages.append((cost, message))
-            messages.sort(key=operator.itemgetter(0), reverse=True)
-            for _, message in messages:
-                print(message)
-
-if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s: %(name)s: %(levelname)s: %(message)s")
-    parser = argparse.ArgumentParser(
-        "Case study of learning to reverse words in a text.",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument(
-        "mode", choices=["train", "sample", "beam_search"],
-        help="The mode to run. In the `train` mode a model is trained."
-             " In the `sample` and `beam_search` modes a trained model is "
-             " to used reverse words in the input text.")
-    parser.add_argument(
-        "save_path", default="reverse_words.tar", nargs='?',
-        help="The path to save the training process if the mode"
-             " is `train` OR path to an `.tar` files with learned"
-             " parameters if the mode is `test`.")
-    parser.add_argument(
-        "--num-batches", default=10000, type=int,
-        help="Train on this many batches.")
-    parser.add_argument(
-        "--data-path",
-        help="text file(s) to read for training, with bash-like expansion"
-             " so wildchars can be used (e.g. data/*.txt)")
-    args = parser.parse_args()
-    if args.data_path:
-        args.data_path = glob.glob(args.data_path)
-    main(**vars(args))
 
